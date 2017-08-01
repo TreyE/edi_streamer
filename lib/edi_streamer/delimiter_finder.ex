1f46e23da51a13bb15128a9bfaa8a8eaa387adb2
Trey Evans <lewis.r.evans@gmail.com>
2017-08-01 12:05:47 -0400

    Now parses delimiters and streams segments.

--- lib/edi_streamer/delimiter_finder.ex
+++ lib/edi_streamer/delimiter_finder.ex
@@ -1,2 +1,32 @@
 defmodule EdiStreamer.DelimiterFinder do
+  @spec find_delimiters(any) :: {:ok, binary, binary} | {:error, term}
+  def find_delimiters(io_thing) do
+    :file.position(io_thing, 0)
+    case :file.pread(io_thing, 3, 1) do
+      :eof -> {:error, :eof}
+      {:error, io_e} -> {:error, {:io_error, io_e}}
+      {:ok, f_sep} ->
+         :file.position(io_thing, 0)
+         find_s_separator(io_thing, f_sep, 0)
+    end
+  end
+
+  defp find_s_separator(io_thing, f_sep, 16) do
+    case IO.binread(io_thing, 2) do
+      :eof -> {:error, :eof}
+      {:error, io_e} -> {:error, {:io_error, io_e}}
+      <<_::8, s_sep::binary>> -> 
+        :file.position(io_thing, 0)
+        {:ok, f_sep, s_sep}
+    end
+  end
+
+  defp find_s_separator(io_thing, f_sep, f_seps_found) do
+    case IO.binread(io_thing, 1) do
+      :eof -> {:error, :eof}
+      {:error, io_e} -> {:error, {:io_error, io_e}}
+      ^f_sep -> find_s_separator(io_thing, f_sep, f_seps_found + 1)
+      _ -> find_s_separator(io_thing, f_sep, f_seps_found)
+    end
+  end
 end
--- test/edi_streamer/streamer_state_test.exs
+++ test/edi_streamer/streamer_state_test.exs
@@ -4,7 +4,8 @@ defmodule EdiStreamer.StreamerStateTest do
 
   test "parse as stream" do
     {:ok, f} = :file.open("b2b_edi.csv", [:read, :binary])
-    state = EdiStreamer.StreamerState.new("*", "~", f)
+    {:ok, f_sep, s_sep} = EdiStreamer.DelimiterFinder.find_delimiters(f)
+    state = EdiStreamer.StreamerState.new(f_sep, s_sep, f)
     stream = EdiStreamer.StreamerState.as_stream(state)
     Enum.each(stream,
     fn x -> IO.puts("Data: #{inspect x}") end)
